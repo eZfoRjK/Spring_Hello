54c7648f3dd24787f9918670d50f8e8d4ec3664b
Alexander Lebedev <ezforjk@yandex.ru>
2024-08-17 18:26:45 +0300

    1st steps

---
+++ src/main/java/Cat.java
@@ -0,0 +1,15 @@
+public class Cat {
+    private String name;
+
+    public Cat() {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String setName(String name) {
+        return this.name = name;
+    }
+}
